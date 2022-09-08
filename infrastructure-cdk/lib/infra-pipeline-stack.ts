cf88adf0bba185cc4eb787bfae99a1373a520d84
Anand Shah <annshah@amazon.lu>
2022-09-08 19:33:29 +0200

    Downgrade lock file and bucket export name fix

--- infrastructure-cdk/lib/infra-pipeline-stack.ts
+++ infrastructure-cdk/lib/infra-pipeline-stack.ts
@@ -3,14 +3,13 @@ import {Construct} from 'constructs';
 import {CodePipeline, CodePipelineSource, ShellStep} from "aws-cdk-lib/pipelines";
 import {RealtimeApplication} from "./real-time-application";
 import {Bucket} from "aws-cdk-lib/aws-s3";
-import {BUCKET_NAME_OUTPUT} from "aws-cdk/lib";
-import {SOURCE_CODE_ZIP} from "./shared-vars";
+import {ASSET_BUCKET_EXPORT_NAME, SOURCE_CODE_ZIP} from "./shared-vars";
 
 export class InfraPipelineStack extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
 
-        const artifactBucket = Bucket.fromBucketName(this, 'artifactBucket-import', Fn.importValue(BUCKET_NAME_OUTPUT))
+        const artifactBucket = Bucket.fromBucketName(this, 'artifactBucket-import', Fn.importValue(ASSET_BUCKET_EXPORT_NAME))
 
         const pipelineName = "blog-infra-pipeline-" + Aws.ACCOUNT_ID + "-" + Aws.REGION;
         const pipeline = new CodePipeline(this, 'Pipeline', {
