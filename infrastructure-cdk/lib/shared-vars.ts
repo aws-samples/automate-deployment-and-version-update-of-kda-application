7945134e3e0aa2ef7156234395f677d07790efe4
Anand Shah <annshah@amazon.lu>
2023-01-26 19:55:54 +0100

    Setting Java as default pipeline mode

--- infrastructure-cdk/lib/shared-vars.ts
+++ infrastructure-cdk/lib/shared-vars.ts
@@ -1,6 +1,6 @@
 import {ApplicationRuntime} from "./constructs/application-runtime";
 
-export const APPLICATION_NAME = "kinesis-analytics-application-python"
-export const BUILD_FOR_RUNTIME = ApplicationRuntime.PYTHON
+export const APPLICATION_NAME = "kinesis-analytics-application"
+export const BUILD_FOR_RUNTIME = ApplicationRuntime.JAVA
 export const SOURCE_CODE_ZIP = "automate-deployment-and-version-update-of-kda-application-main.zip"
 export const ASSET_BUCKET_EXPORT_NAME = "Blog::Artifact::BucketName"
