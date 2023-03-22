49c71176e55226a4a5c9892b38c58d7cb2723076
Luis Morales <moralesl@users.noreply.github.com>
2023-03-22 15:08:00 +0100

    Make manual approval configurable

--- infrastructure-cdk/lib/shared-vars.ts
+++ infrastructure-cdk/lib/shared-vars.ts
@@ -4,3 +4,4 @@ export const APPLICATION_NAME = "kinesis-analytics-application"
 export const BUILD_FOR_RUNTIME = ApplicationRuntime.JAVA
 export const SOURCE_CODE_ZIP = "automate-deployment-and-version-update-of-kda-application-main.zip"
 export const ASSET_BUCKET_EXPORT_NAME = "Blog::Artifact::BucketName"
+export const MANUAL_APPROVAL_REQUIRED = true
